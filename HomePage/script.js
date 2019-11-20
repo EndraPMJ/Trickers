485085183f1916973109e328e6e432ccf773ad14
Nur Endra Purwa Maulana Jati <endrapmj@gmail.com>
2019-11-20 15:40:52 +0700

    Trickers

---
+++ HomePage/script.js
@@ -0,0 +1,43 @@
+const navSlide = ()=>{
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li');
+
+    burger.addEventListener('click',()=>{
+        nav.classList.toggle('nav-active');
+    });
+
+    navLinks.forEach((link, index) => {
+        link.style.animation = 'navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s';
+    })
+
+}
+navSlide();
+
+/*kata mereka*/
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
